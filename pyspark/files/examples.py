47e790d7b5cf86fea54d81d61e257148977e65a9
Wan Bachtiar <sindbach@gmail.com>
2016-10-11 14:06:14 +1100

    Add csv loading example

--- pyspark/files/examples.py
+++ pyspark/files/examples.py
@@ -1,6 +1,6 @@
 
 import sys
-from pyspark.sql import SQLContext
+from pyspark.sql import SQLContext, SparkSession
 from pyspark import SparkContext, SparkConf
 
 sparkConf = SparkConf().setMaster("local").setAppName("MongoSparkConnectorTour").set("spark.app.id", "MongoSparkConnectorTour")
@@ -16,6 +16,8 @@ df = sqlContext.read.format("com.mongodb.spark.sql.DefaultSource")\
 
 # print data frame schema
 df.printSchema()
+# print first dataframe row
+df.first()
 
 # convert dataframe to rdd 
 rdd = df.rdd 
@@ -26,7 +28,9 @@ pipeline = [{'$sort': {'timestamp': 1}},
             {'$group':{'_id':{'myid':'$myid'}, 'record':{'$first':'$$ROOT'}}}, 
             {'$project':{'_id':0, 'doc':'$record.doc', 'timestamp':'$record.timestamp', 'myid':'$record.myid'}}
             ]
-df_pipeline = sqlContext.read.format("com.mongodb.spark.sql.DefaultSource").option("pipeline", pipeline).load()
+df_pipeline = sqlContext.read.format("com.mongodb.spark.sql.DefaultSource")\
+                             .option("spark.mongodb.input.uri", "mongodb://mongodb:27017/spark.times")\
+                             .option("pipeline", pipeline).load()
 df_pipeline.first()
 
 # Filter by Integer and by String
@@ -49,6 +53,13 @@ df = sqlContext.read.format("com.mongodb.spark.sql.DefaultSource")\
                     .option("spark.mongodb.input.uri", "mongodb://mongodb:27017/spark.output")\
                     .load()
 df.show()
+
+# Read a csv file and store into MongoDB 
+spark = SparkSession.builder.appName("MongoSparkConnectorTour").getOrCreate()
+dataframe = spark.read.csv("./population_by_country_1980_2010.csv", header=True, mode="DROPMALFORMED")
+dataframe.write.format("com.mongodb.spark.sql.DefaultSource")\
+               .option("spark.mongodb.output.uri", "mongodb://mongodb:27017/spark.population")\
+               .save()
 
 print("Done")
 sys.exit(0)
